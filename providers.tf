541d59986fb3b446a82b92bc37b34decb48a3334
GauriNandan99 <arjunaparakram99@gmail.com>
2025-10-26 05:25:23 +0530

    added backend

--- providers.tf
+++ providers.tf
@@ -7,6 +7,14 @@ terraform {
   }
 }
 
+backend "s3" {
+  dynamodb_table = "demoterraformstate"
+  bucket = "demotfbackend"
+  key = "demo/terraform.tfstate"
+  region = "ap-south-1"
+}
+
+
 provider "aws" {
   region = var.region
 }
